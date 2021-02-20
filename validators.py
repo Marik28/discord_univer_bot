f173cbcb1b2ec306a8b6b6b9bc0b78525c96f36b
Marik28 <markel20002000@gmail.com>
2021-02-20 19:09:47 +0600

    знак "%" теперь проходит валидацию

--- validators.py
+++ validators.py
@@ -7,7 +7,7 @@ def is_valid_image_link(link: str) -> bool:
     link_pattern = re.compile(r"^(?P<protocol>http|https)://"
                               r"(?P<domain_and_path>[\w._~:/?#\[\]@!$&'()*+,;=-]+)"
                               r"(?P<img_extension>\.(png|jpeg|jpg))"
-                              r"(?P<smth_else>[\w._~:/?#\[\]@!$&'()*+,;=-]+)?$")
+                              r"(?P<smth_else>[\w._~:/?#\[\]@!%$&'()*+,;=-]+)?$")
     if link_pattern.match(link) is None:
         return False
     else:
