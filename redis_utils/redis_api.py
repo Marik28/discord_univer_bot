a425de3675cb849f27c1e8bc516e6b747f11636c
Marik28 <markel20002000@gmail.com>
2021-02-21 17:22:17 +0600

    добавил класс RedisManager, теперь все круто

--- redis_utils/redis_api.py
+++ redis_utils/redis_api.py
@@ -1,37 +1,48 @@
 import redis
 
-from constants import ANIME_LINKS_DB, ANIME_LINKS_FILE
+from config import ANIME_LINKS_DB, ANIME_LINKS_SET
 from exceptions import InvalidImageLink
 from validators import is_valid_image_link
 
-ANIME_LINKS_LIST = "anime_links"
+
+def decode_string(func):
+    """Декоратор для декодирования результата функций, возвращающих bytes, в str """
+    def wrapper(*args, **kwargs) -> str:
+        return func(*args, **kwargs).decode()
+
+    return wrapper
 
 
-def init_redis(filename: str) -> redis.Redis:
-    with open(filename, "r", encoding="utf-8") as file:
-        links = [link.strip() for link in file.readlines()]
-    connection = redis.Redis(host="localhost", port=6379, db=ANIME_LINKS_DB)
-    connection.flushdb()
-    connection.sadd(ANIME_LINKS_LIST, *links)
+class RedisManager:
+    """Класс для упрощенного управления БД Redis"""
+
+    def __init__(self, connection: redis.Redis, links_set_name: str = None):
+        self._connection = connection
+        self._links_set_name = links_set_name
+
+    def add_link_to_set(self, link: str) -> str:
+        """Добавляет ссылку во множество Redis, возвращает сообщение об успешности добавления.
+        Если сслыка не валидна, кидает исключение InvaliImageLink"""
+
+        if not is_valid_image_link(link):
+            raise InvalidImageLink("Ссылка не является правильной :( (по крайней мере она не прошла нашу проверку)")
+        else:
+            if self._connection.sismember(self._links_set_name, link):
+                msg = 'Картинка уже есть в нашей базе'
+            else:
+                self._connection.sadd(self._links_set_name, link)
+                msg = 'Картинка добавлена в базу успешно!'
+        return msg
+
+    @decode_string
+    def get_random_link(self):
+        """Возвращает случайную ссылку из множества ссылок"""
+        return self._connection.srandmember(self._links_set_name)
+
+
+def get_redis_connection(host="localhost", port=6379, db=ANIME_LINKS_DB) -> redis.Redis:
+    connection = redis.Redis(host=host, port=port, db=db)
     return connection
 
 
-connection = init_redis(ANIME_LINKS_FILE)
-
-
-def add_link_to_redis(link: str) -> str:
-    if not is_valid_image_link(link):
-        raise InvalidImageLink("Ссылка не является правильной :( (по крайней мере она не прошла нашу проверку)")
-    else:
-        if connection.sismember(ANIME_LINKS_LIST, link):
-            msg = 'Картинка уже есть в нашей базе'
-        else:
-            connection.sadd(ANIME_LINKS_LIST, link)
-            with open(ANIME_LINKS_FILE, "a", encoding="utf-8") as file:
-                file.write(f"\n{link}")
-                msg = 'Картинка добавлена в базу успешно!'
-    return msg
-
-
-def get_random_link() -> str:
-    return connection.srandmember(ANIME_LINKS_LIST).decode()
+redis_conn = get_redis_connection()
