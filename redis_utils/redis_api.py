e5e7e77643c4a7234db252dbdeb640dc97d9e5cf
Marik28 <markel20002000@gmail.com>
2021-02-24 00:48:51 +0600

    переделал RedisSetManager, теперь он по API подобен встроенному set

--- redis_utils/redis_api.py
+++ redis_utils/redis_api.py
@@ -1,73 +1,83 @@
+from typing import Iterable, Union
+
 from redis import Redis
 
-from config import ANIME_LINKS_DB, ANIME_LINKS_SET
+from config import ANIME_LINKS_DB, ANIME_LINKS_SET, REDIS_PORT, REDIS_HOST
 from exceptions import InvalidImageLink
 from validators import is_valid_image_link
 
 
-def decode_string(func):
-    """Декоратор для декодирования результата функций, возвращающих bytes, в str """
-
-    def wrapper(*args, **kwargs) -> str:
-        result = func(*args, **kwargs).decode()
-        if isinstance(result, bytes):
-            result.decode()
-        return result
-
-    return wrapper
+# не знаю, нужно ли это пока что
+# class BaseRedisCollectionManager:
+#     """Базовый класс для управления коллекциями Redis"""
+#     def __init__(self, connection: Redis):
+#         self._connection = connection
+#
+#     def close_connection(self):
+#         """Закрывает соединение с Redis"""
+#         self._connection.close()
 
 
 class RedisSetManager:
     """Класс для упрощенного управления конкретным множеством в Redis"""
 
-    def __new__(cls, *args, **kwargs):
-        """Синглтон, чтобы было, а шо"""
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(RedisSetManager, cls).__new__(cls)
-        return cls.instance
-
     def __init__(self, connection: Redis, set_name: str):
         self._connection = connection
         self._set_name = set_name
 
-    def add_values(self, members: list[str]):
+    def update(self, values: Iterable) -> None:
         """Добавляет несколько значений во множество"""
-        return self._connection.sadd(self._set_name, *members)
+        self._connection.sadd(self._set_name, *values)
 
-    def add_value(self, member: str):
+    def add(self, value: str) -> None:
         """Добавляет значение во множество"""
-        return self._connection.sadd(self._set_name, member)
+        self._connection.sadd(self._set_name, value)
 
-    @decode_string
-    def get_random_value(self):
-        """Возвращает случайную ссылку из множества ссылок"""
-        return self._connection.srandmember(self._set_name)
+    def get_random_value(self) -> Union[str, None]:
+        """Возвращает случайное значение из множества. Если множество пустое, возвращает None"""
+        random_value: bytes = self._connection.srandmember(self._set_name)
+        if random_value is None:
+            return random_value
+        else:
+            return random_value.decode()
 
     def clear(self):
         """Очищает множество (== удаляет его)"""
         self._connection.delete(self._set_name)
 
+    def close_connection(self):
+        """Закрывает соединение с Redis"""
+        self._connection.close()
+
+    def pop(self) -> Union[str, None]:
+        """Удаляет и возвращает случайное значение из множества. Если множество пустое, возвращает None"""
+        random_value: bytes = self._connection.spop(self._set_name)
+        if random_value is None:
+            return random_value
+        else:
+            return random_value.decode()
+
+    def __len__(self):
+        """Возвращает количество элементов множества"""
+        return self._connection.scard(self._set_name)
+
 
 class LinksSetManager(RedisSetManager):
 
     def add_link(self, link: str, validate: bool = True) -> str:
-        """Добавляет ссылку во множество Redis, возвращает сообщение об успешности добавления.
+        """Добавляет ссылку во множество Redis. Возвращает сообщение об успешном добавлении,
+        даже если ссылка уже лежала во множестве.
         Если сслыка не валидна, кидает исключение InvaliImageLink"""
 
         if validate and not is_valid_image_link(link):
             raise InvalidImageLink("Ссылка не является правильной :( (по крайней мере она не прошла нашу проверку)")
-
-        result: int = self.add_value(link)
-        if result == 0:
-            msg = 'Картинка уже есть в нашей базе'
-        else:
-            msg = 'Картинка добавлена в базу успешно!'
-        return msg
+        self.add(link)
+        return 'Картинка добавлена в базу успешно!'
 
 
-def get_redis_connection(host="localhost", port=6379, db=ANIME_LINKS_DB) -> Redis:
+def get_redis_connection(host=REDIS_HOST, port=REDIS_PORT, db=0) -> Redis:
     return Redis(host=host, port=port, db=db)
 
 
-redis_conn = get_redis_connection()
+redis_conn = get_redis_connection(db=ANIME_LINKS_DB)
 links_set_manager = LinksSetManager(redis_conn, ANIME_LINKS_SET)
