c76abf1835df6402a7502bfb88b1934565d72496
Marik28 <69481006+Marik28@users.noreply.github.com>
2021-02-23 10:55:39 +0600

    улучшил функцию валидации

--- validators.py
+++ validators.py
@@ -8,7 +8,4 @@ def is_valid_image_link(link: str) -> bool:
                               r"(?P<domain_and_path>[\w._~:/?#\[\]@!$&'()*+,;=-]+)"
                               r"(?P<img_extension>\.(png|jpeg|jpg))"
                               r"(?P<smth_else>[\w._~:/?#\[\]@!%$&'()*+,;=-]+)?$")
-    if link_pattern.match(link) is None:
-        return False
-    else:
-        return True
+    return link_pattern.match(link) is not None
