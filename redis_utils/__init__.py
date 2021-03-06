f8e8b077e4fa3d4dae04630b53f0bd23d491846c
Marik28 <markel20002000@gmail.com>
2021-03-06 12:50:03 +0600

    по мелочи

--- redis_utils/__init__.py
+++ redis_utils/__init__.py
@@ -6,10 +6,12 @@ from redis_utils.redis_set import LinksSetManager, RedisSetManager
 
 test_set_name = "test_set"
 test_hash_name = "test_hash"
-
+test_counter_name = "test_counter"
 
 redis_conn = get_redis_connection(db=ANIME_LINKS_DB)
 links_set_manager = LinksSetManager(redis_conn, ANIME_LINKS_SET)
-test_set = RedisSetManager(redis_conn, test_set_name)
-test_hash = RedisHashManager(redis_conn, "test_hash")
 rolls_counter = CounterHashManager(redis_conn, "rolls")
+
+test_set = RedisSetManager(redis_conn, test_set_name)
+test_hash = RedisHashManager(redis_conn, test_hash_name)
+test_counter = CounterHashManager(redis_conn, test_counter_name)
--- write_data.py
+++ write_data.py
@@ -1,17 +1,17 @@
 from config import BASE_DIR, ANIME_LINKS_SET
-from redis_utils import redis_api
+from redis_utils import RedisSetManager, get_redis_connection
 
 links_file = BASE_DIR / 'redis_utils' / 'links.txt'
 
 
-def save_links_to_redis(file_with_links, redis_set: redis_api.RedisSetManager):
+def save_links_to_redis(file_with_links, redis_set: RedisSetManager):
     with open(file_with_links, 'r') as file:
         links = [link.strip() for link in file.readlines()]
     redis_set.update(links)
 
 
 if __name__ == '__main__':
-    conn = redis_api.get_redis_connection()
-    r_manager = redis_api.RedisSetManager(conn, ANIME_LINKS_SET)
+    conn = get_redis_connection()
+    r_manager = RedisSetManager(conn, ANIME_LINKS_SET)
     save_links_to_redis(links_file, r_manager)
     r_manager.close_connection()
